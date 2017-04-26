2c35851718c5d668d7f5b0eaad78f4f7a43d9526
yaaroslaw <yaaroslaw@gmail.com>
2017-04-26 18:08:11 +0300

    Added a project for UnitTests

---
+++ LogAndNotificationUnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAndNotificationUnitTests
+{
+    public class LogAnalyzerTests
+    {/// <summary>
+     /// Code convention: [UnitOfWorkName]_[ScenarioUnderTest]_[ExpectedBehavior].
+    /// </summary>
+         public void IsLogFileNameValid_BadExtension_ReturnsFalse()
+        {
+
+        }
+    }
+}
