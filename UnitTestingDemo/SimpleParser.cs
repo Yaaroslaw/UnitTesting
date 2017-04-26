9a64cad0e0446a4631e30bc96057e0bf682f3648
yaaroslaw <yaaroslaw@gmail.com>
2017-04-26 15:06:05 +0300

    Added Parser class

---
+++ UnitTestingDemo/SimpleParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingDemo
+{
+    public class SimpleParser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+                return 0;
+            else if (!numbers.Contains(","))
+                return int.Parse(numbers);
+            else
+                throw new NotImplementedException("Wright now can only parse zero or one number");
+               
+        }
+    }
+}
