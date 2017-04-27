cefff4728f3a29fe3a77e82a671a2e375887f15f
yaaroslaw <yaaroslaw@gmail.com>
2017-04-27 10:51:51 +0300

    Added a project for NUnit Testing, needed references

---
+++ LogAndNotoficationUnitTestsNUNIT/LogAnalyzerTestsNUNIT.cs
@@ -0,0 +1,42 @@
+﻿using LogAndNotification;
+using NUnit.Framework;
+
+namespace LogAndNotoficationUnitTestsNUNIT
+{
+    [TestFixture]
+    public class LogAnalyzerTestsNUNIT
+    {
+        [Test]
+        public void IsLogFileNameValid_BadExtension_ReturnsFalse()
+        {
+            // Arrange
+            LogAnalyzer analyzer = new LogAnalyzer();
+            //Act
+            var result = analyzer.IsLogFileNameValid("invalidFileName.Foo");
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsLogFileNameValid_GoodExtensionLowerCase_ReturnsTrue()
+        {
+            //Arrange
+            LogAnalyzer analyzer = new LogAnalyzer();
+            //Act
+            var result = analyzer.IsLogFileNameValid("fileWithAGoodExtensionLowercase.slf");
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsLogFileNameValid_GoodExtensionUpperCase_ReturnsTrue()
+        {
+            //Arrange
+            LogAnalyzer analyzer = new LogAnalyzer();
+            //Act
+            var result = analyzer.IsLogFileNameValid("fileWithAGoodExtentionUpperCase.SLF");
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
